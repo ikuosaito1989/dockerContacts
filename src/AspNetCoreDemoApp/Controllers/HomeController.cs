c820bdf25a2024fcbf0b84568bfe83bd472a3b5e
斉藤育男 <ikuo@saitouikuonoMacBook-ea.local>
2018-06-28 23:03:04 +0900

    初回コミット

---
+++ src/AspNetCoreDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using src.Manager;
+
+namespace src.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index () {
+            var model = new ContactViewModel ();
+            return View (model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index (ContactViewModel model) {
+            if (ModelState.IsValid) {
+                var mailManager = new MailManager {
+                    Subject = "[ITunEsTooL]お問い合わせがありました。",
+                    Body = string.Format ("{0}\n{1}\n{2}", model.Name, model.Email, model.Message)
+                };
+                await mailManager.SendEmailAsync ();
+                model = new ContactViewModel ();
+                model.Complite = "お問い合わせありがとうございました。ご回答にはしばらくお待ち下さい。";
+            }
+
+            return View (model);
+        }
+    }
+
+    public class ContactViewModel {
+
+        [Required (ErrorMessage = "名前は必須です。")]
+        public string Name { get; set; }
+
+        [Required (ErrorMessage = "メールアドレスは必須です。")]
+        [EmailAddress (ErrorMessage = "Emailを入力してください。")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage = "お問い合わせ内容は必須です。")]
+        public string Message { get; set; }
+        public string Complite { get; set; }
+    }
+}
---
+++ src/AspNetCoreDemoApp/Controllers/MailManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace src.Manager {
+    public class MailManager {
+
+        public MailManager () {
+            this.Host = "smtp.gmail.com";
+            this.Port = 587;
+            this.UserName = "itunestool.jp@gmail.com";
+            this.PassWord = "irxgbtffgglufwot";
+            this.From = "itunestool.jp@gmail.com";
+            this.To = "itunestool.jp@gmail.com";
+        }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+
+        public async Task SendEmailAsync () {
+
+            var emailMessage = new MimeMessage ();
+
+            emailMessage.From.Add (new MailboxAddress (this.UserName, this.From));
+
+            emailMessage.To.Add (new MailboxAddress (this.To));
+
+            emailMessage.Subject = this.Subject;
+
+            emailMessage.Body = new TextPart ("plain") { Text = this.Body };
+
+            using (var client = new SmtpClient ()) {
+
+                await client.ConnectAsync (this.Host, this.Port, SecureSocketOptions.StartTls);
+                await client.AuthenticateAsync (this.UserName, this.PassWord);
+                await client.SendAsync (emailMessage);
+                await client.DisconnectAsync (true);
+            }
+        }
+    }
+
+}
